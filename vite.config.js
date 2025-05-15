389bce8b4be3fbc07ea4f725977cb3f5a1c52b37
Tio Muhamad Nur <outsider.kuningan52@gmail.com>
2025-05-15 11:54:12 +0700

    2nd commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: process.env.VITE_APP_URL ? `${process.env.VITE_APP_URL}/` : '/',
     plugins: [
         laravel({
             input: [
